2b08584912336bd937b20f7e2ee5d8f5a7407bbe
biiit4894 <ghiesf@naver.com>
2023-04-18 16:11:43 +0900

    [FEAT] 로컬 테스트용 서버 구혀

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import router from "./router";
+import app from "./app";
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/test", (req: Request, res: Response) => {
+  res.send("server is listening on 3000");
+});
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
