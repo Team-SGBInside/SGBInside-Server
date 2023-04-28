17f1130160e53fe42f29f7cde0f7096fb0d0c5c8
biiit4894 <ghiesf@naver.com>
2023-04-29 02:08:10 +0900

    [FIX] 유저 생성 서비스 bcrypt, DTO 적용

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,23 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { UserCreateDTO } from "../interfaces/UserCreateDTO";
 const prisma = new PrismaClient();
 
 // 유저 생성
-const createUser = async (
-  loginId: string,
-  password: string,
-  name: string,
-  school: string,
-  grade: number,
-  isTeen: boolean
-) => {
+const createUser = async (userCreateDTO: UserCreateDTO) => {
+  // 넘겨받은 password를 bcrypt의 도움을 받아 암호화
+  const salt = await bcrypt.genSalt(10); // 매우 작은 임의의 랜덤 텍스트 salt
+  const password = await bcrypt.hash(userCreateDTO.password, salt); // 위에서 랜덤을 생성한 salt를 이용해 암호화
+
   const data = await prisma.user.create({
     data: {
-      loginId,
+      loginId: userCreateDTO?.loginId,
+      name: userCreateDTO?.name,
+      school: userCreateDTO?.school,
+      grade: userCreateDTO?.grade,
+      isTeen: userCreateDTO?.isTeen,
       password,
-      name,
-      school,
-      grade,
-      isTeen,
     },
   });
 
