bd32f45488d688ca1ddf57cd695b606d7f87a537
biiit4894 <ghiesf@naver.com>
2023-04-29 01:48:56 +0900

    [ADD] express-validator 미들웨어 추가

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { userController } from "../controller";
 import { auth } from "../middlewares";
+import { body } from "express-validator";
 
 const router: Router = Router();
 
 // 유저 생성 - POST ~/auth
-router.post("/", auth, userController.createUser);
+router.post(
+  "/",
+  [
+    body("loginId").trim().notEmpty(),
+    body("password").trim().notEmpty(),
+    body("name").trim().notEmpty(),
+    body("school").trim().notEmpty(),
+    body("grade").trim().notEmpty(),
+    body("isTeen").trim().notEmpty(),
+  ],
+  auth,
+  userController.createUser
+);
 
 export default router;
