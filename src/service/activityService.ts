82fc7b3f4d008314c48c6f7d8198144fbff36293
biiit4894 <ghiesf@naver.com>
2023-05-01 04:37:52 +0900

    [FIX] 서비스 DTO 적용

--- src/service/activityService.ts
+++ src/service/activityService.ts
@@ -1,28 +1,26 @@
+import { CreativeActivityCreateDTO } from "./../interfaces/activity/CreativeActivityCreateDTO";
 import { SubjectDetailedActivityCreateDTO } from "./../interfaces/activity/SubjectDetailedActivityCreateDTO";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // 창의적 체험활동 기록
 const createCreativeActivity = async (
-  userId: number,
-  name: string,
-  activityType: string,
-  date: string,
-  semester: string,
-  role: string,
-  thoughts: string
+  creativeActivityCreateDTO: CreativeActivityCreateDTO
 ) => {
-  await prisma.creative_Activity.create({
+  const data = await prisma.creative_Activity.create({
     data: {
-      name,
-      activityType,
-      date,
-      semester,
-      role,
-      thoughts,
-      writerId: userId,
+      writerId: creativeActivityCreateDTO.userId,
+      name: creativeActivityCreateDTO.name,
+      activityType: creativeActivityCreateDTO.activityType,
+      startDate: creativeActivityCreateDTO.startDate,
+      endDate: creativeActivityCreateDTO.endDate,
+      semester: creativeActivityCreateDTO.semester,
+      role: creativeActivityCreateDTO.role,
+      thoughts: creativeActivityCreateDTO.thoughts,
     },
   });
+  const activityId = data.activityId;
+  return { activityId };
 };
 
 // 과목별 세부능력 특기사항 활동 기록
