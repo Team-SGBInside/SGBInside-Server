4645c81e43d440731fa0a45e296bfcde55d08582
biiit4894 <ghiesf@naver.com>
2023-05-01 03:44:57 +0900

    [ADD] string 필드에 validator 추가

--- src/router/activityRouter.ts
+++ src/router/activityRouter.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
 import { activityController } from "../controller";
+import auth from "../middlewares/auth";
+import { body } from "express-validator";
+
 const router: Router = Router();
 
 router.post("/creative", activityController.createCreativeActivity);
-router.post("/subject", activityController.createSubjectDetailedActivity);
+router.post(
+  "/subject",
+  [
+    body("subjectName").trim().notEmpty(),
+    body("subjectContent").trim().notEmpty(),
+    body("activitySemester").trim().notEmpty(),
+    body("startDate").trim().notEmpty(),
+    body("endDate").trim().notEmpty(),
+    body("mainActivity").trim().notEmpty(),
+    body("activityContentDetail").trim().notEmpty(),
+  ],
+  activityController.createSubjectDetailedActivity
+);
 export default router;
