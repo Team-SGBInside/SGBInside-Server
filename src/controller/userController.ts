838f48ec2335f9b875789a1f7f8e508cec460248
biiit4894 <ghiesf@naver.com>
2023-05-08 15:57:20 +0900

    [ADD] 서버 내부 에러 시 분기처리 추가

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -18,26 +18,37 @@ const createUser = async (req: Request, res: Response) => {
   }
 
   const userCreateDTO: UserCreateDTO = req.body;
-  const data = await userService.createUser(userCreateDTO);
 
-  if (!data) {
+  try {
+    const data = await userService.createUser(userCreateDTO);
+
+    if (!data) {
+      return res
+        .status(sc.BAD_REQUEST)
+        .send(fail(sc.BAD_REQUEST, rm.SIGNUP_FAIL));
+    }
+
+    // jwtHandler 내 sign 함수를 이용해 accessToken 생성
+    const accessToken = jwtHandler.sign(data.userId);
+
+    const result = {
+      userId: data.userId,
+      name: data.name,
+      accessToken,
+    };
+
     return res
-      .status(sc.BAD_REQUEST)
-      .send(fail(sc.BAD_REQUEST, rm.SIGNUP_FAIL));
+      .status(sc.CREATED)
+      .send(success(sc.CREATED, rm.SIGNUP_SUCCESS, result));
+
+  } catch(e) {
+    console.log(error);
+    // 서버 내부에서 오류 발생
+    res
+      .status(sc.INTERNAL_SERVER_ERROR)
+      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
   }
-
-  // jwtHandler 내 sign 함수를 이용해 accessToken 생성
-  const accessToken = jwtHandler.sign(data.userId);
-
-  const result = {
-    userId: data.userId,
-    name: data.name,
-    accessToken,
-  };
-
-  return res
-    .status(sc.CREATED)
-    .send(success(sc.CREATED, rm.SIGNUP_SUCCESS, result));
+  
 };
 
 // 로그인
