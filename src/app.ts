3ba4e9155be1e978eb2dac836b2adfbb29d75fad
biiit4894 <ghiesf@naver.com>
2023-05-25 16:34:04 +0900

    [FIX] 회원가입 시 cors 에러 해결

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json()); // bodyParser가 express 최근 버전에서 deprecated되어서 다음과 같이 처리해줘야함
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get("/test", (req: Request, res: Response) => {
   res.send("server is listening on 3000");
