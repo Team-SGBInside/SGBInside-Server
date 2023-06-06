ab40e3c9781e101c8c8b6cb5f71be3f8545b1d27
biiit4894 <ghiesf@naver.com>
2023-06-06 20:01:17 +0900

    [FEAT] 마이페이지 생기부 항목 유형에 따라 활동 조회

--- src/controller/mypageController.ts
+++ src/controller/mypageController.ts
@@ -4,21 +4,47 @@ import { sc, rm } from "../constants";
 import { fail, success } from "../constants/response";
 import { mypageService } from "../service";
 
+const sortType = {
+  ALL: "all",
+  CREATIVE: "creative",
+  SUBJECT: "subject",
+  PRIZE: "prize",
+  BOOK: "book",
+};
+
 const getMypage = async (req: Request, res: Response) => {
+  const sort = req.query.sort as string;
   const userId = req.body.userId;
-  if (!userId) {
+
+  if (!userId || !sort) {
     return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
   }
+
+  if (
+    sort !== sortType.ALL &&
+    sort !== sortType.CREATIVE &&
+    sort !== sortType.SUBJECT &&
+    sort !== sortType.PRIZE &&
+    sort !== sortType.BOOK
+  ) {
+    return res
+      .status(sc.BAD_REQUEST)
+      .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+  }
+
   try {
     const accountInfo = await mypageService.getAccountInfoByUserId(userId);
     //const allActivity = await mypageService.getActivityByUserId(+userId);
-    const totalActivity = await mypageService.getTotalActivityByUserId(userId);
+    const totalActivity = await mypageService.getTotalActivityByUserId(
+      userId,
+      sort
+    );
 
-    const activityCount =
-      totalActivity.allcreativeActivity.length +
-      totalActivity.allSubjectDetailedActivity.length +
-      totalActivity.allPrizeActivity.length +
-      totalActivity.allBookActivity.length;
+    // const activityCount =
+    //   totalActivity.allcreativeActivity.length +
+    //   totalActivity.allSubjectDetailedActivity.length +
+    //   totalActivity.allPrizeActivity.length +
+    //   totalActivity.allBookActivity.length;
 
     const data = {
       userId: accountInfo?.userId,
@@ -26,7 +52,7 @@ const getMypage = async (req: Request, res: Response) => {
       school: accountInfo?.school,
       grade: accountInfo?.grade,
       age: accountInfo?.age,
-      activityCount: activityCount,
+      // activityCount: activityCount,
       totalActivity: totalActivity,
     };
     return res
--- src/service/mypageService.ts
+++ src/service/mypageService.ts
@@ -12,23 +12,63 @@ const getAccountInfoByUserId = async (userId: number) => {
   return accountInfo;
 };
 
-const getTotalActivityByUserId = async (userId: number) => {
+const getTotalActivityByUserId = async (userId: number, sort: string) => {
+  if (sort !== "all") {
+    switch (sort) {
+      case "creative":
+        const allcreativeActivity = await prisma.creative_Activity.findMany({
+          where: {
+            writerId: userId,
+          },
+        });
+        return allcreativeActivity;
+
+      case "subject":
+        const allSubjectDetailedActivity =
+          await prisma.subject_Detailed_Ability_Activity.findMany({
+            where: {
+              writerId: userId,
+            },
+          });
+        return allSubjectDetailedActivity;
+
+      case "prize":
+        const allPrizeActivity = await prisma.prize_Activity.findMany({
+          where: {
+            writerId: userId,
+          },
+        });
+        return allPrizeActivity;
+
+      case "book":
+        const allBookActivity = await prisma.book_Activity.findMany({
+          where: {
+            writerId: userId,
+          },
+        });
+        return allBookActivity;
+    }
+  }
+
   const allcreativeActivity = await prisma.creative_Activity.findMany({
     where: {
       writerId: userId,
     },
   });
+
   const allSubjectDetailedActivity =
     await prisma.subject_Detailed_Ability_Activity.findMany({
       where: {
         writerId: userId,
       },
     });
+
   const allPrizeActivity = await prisma.prize_Activity.findMany({
     where: {
       writerId: userId,
     },
   });
+
   const allBookActivity = await prisma.book_Activity.findMany({
     where: {
       writerId: userId,
