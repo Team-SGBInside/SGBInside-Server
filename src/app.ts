57b689296ed6aa0329998c114b5e116aa4d4d471
biiit4894 <ghiesf@naver.com>
2023-08-09 18:04:04 +0900

    [CHORE] cors origin list 추가

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ const app = express();
 const originList = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "http://localhost:3002",
+  "http://localhost:3003",
+  "http://localhost:3004",
   "http://localhost:3005",
 ];
 
